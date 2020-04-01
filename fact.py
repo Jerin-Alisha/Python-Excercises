1bed80c9e9630ab56c535fc0cb91e8d5f43669cf
Jerin-Alisha <44497720+Jerin-Alisha@users.noreply.github.com>
2020-04-01 12:45:41 +0530

    Add files via upload

---
+++ fact.py
@@ -0,0 +1,5 @@
+f=1
+num=input("enter a number:")
+for i in range(1,num+1):
+    f=f*i
+print f
---
+++ star triangle.py
@@ -0,0 +1,11 @@
+for i in range(0,6):
+    for j in range(0,6-i):
+        print " ",
+    for j in range(0,i):
+        print "*  ",
+    print
+    
+        
+
+
+    
